ef160620c2a88d79056d2f2880745b1fb6097324
Marcin Kisiel <kisiel.marcin@gmail.com>
2021-11-13 12:54:52 +0100

    Add SCSS architecture and basic Gulp config file

---
+++ gulpfile.js
@@ -0,0 +1,31 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+const concat = require('gulp-concat');
+const browserSync = require('browser-sync').create();
+
+function style() {
+	return gulp
+		.src('./scss/**/*.scss')
+		.pipe(sass({ outputStyle: 'compressed' }).on('error', sass.logError))
+		.pipe(concat('main.css'))
+		.pipe(gulp.dest('./css/'))
+		.pipe(browserSync.stream());
+}
+
+function watch() {
+	browserSync.init({
+		server: {
+			baseDir: './',
+		},
+		notify: true,
+	});
+	gulp.watch('./scss/**/*.scss', style);
+	// gulp.watch('./css/*.css').on('change', browserSync.reload);
+	gulp.watch('./*.php').on('change', browserSync.reload);
+	gulp.watch('./*.html').on('change', browserSync.reload);
+	gulp.watch('./js/**/*.js').on('change', browserSync.reload);
+}
+
+exports.style = style;
+exports.watch = watch;
+exports.default = watch;
